59f932ff3a9c73a6f4eb078a0e1d41019e74507e
sergeylavroff <WouxXerxLen9>
2021-02-22 20:20:04 +0300

    Final

--- Practice.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-r = requests.get(
-    'https://baconipsum.com/api/?type=all-meat&paras=1&start-with-lorem=0&format=json')  # делаем запрос на сервер по переданному адресу
-texts = json.loads(r.content)
-print(type(texts))
-print(texts[0])
